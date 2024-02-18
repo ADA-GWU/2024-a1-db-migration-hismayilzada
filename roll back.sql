89133904cc4790c460edc736cc7383fc9597bdec
Humay <73535604+hismayilzada@users.noreply.github.com>
2024-02-18 17:20:06 +0400

    Update roll back.sql

--- roll back.sql
+++ roll back.sql
@@ -1,4 +1,9 @@
 alter table STUDENTS drop column STUDENT_ID;
+
 alter table STUDENTS drop column ST_NAMEE;
 alter table STUDENTS drop column ST_LASTT;
-alter table INTERESTS drop column INTEREST;
+
+alter table INTERESTS add column INTEREST varchar(20);
+insert into INTERESTS(STUDENT_ID, INTEREST) select STUDENT_ID, unnest(INTERESTS) from INTERESTS;
+delete from INTERESTS where INTERESTS.INTEREST is null;
+alter table INTERESTS drop column INTERESTS;
