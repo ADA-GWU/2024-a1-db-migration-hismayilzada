9bcb60cc9eb4162d6d872c80d0c578aff78b746d
Humay <73535604+hismayilzada@users.noreply.github.com>
2024-02-18 17:12:40 +0400

    Update migrate.sql

--- migrate.sql
+++ migrate.sql
@@ -5,7 +5,7 @@ update STUDENTS set STUDENT_ID = ST_ID;
 alter table STUDENTS add ST_NAMEE varchar(30);
 update STUDENTS set ST_NAMEE = ST_NAME;
 alter table STUDENTS add ST_LASTT varchar(30);
-update STUDENTS set ST_LAST1 = ST_LAST;
+update STUDENTS set ST_LASTT = ST_LAST;
 
 alter table INTERESTS add column INTERESTS varchar(20)[];
 insert into INTERESTS(STUDENT_ID, INTERESTS) select STUDENT_ID, array_agg(INTEREST) from INTERESTS group by(STUDENT_ID);
